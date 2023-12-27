336b016d4dc48b41f2cb2d1b401a5ac506e651ac
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-27 13:35:49 -0300

    Ruta protegida si no hay nada para borrar

--- src/vistas/BorrarPreguntas.tsx
+++ src/vistas/BorrarPreguntas.tsx
@@ -1,10 +1,18 @@
 import useGlobalStore from "../hooks/useGlobalStore";
 import CardBorrarPregunta from "../components/CardBorrarPregunta";
 import { Pregunta } from "../interface/Pregunta";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function BorrarPreguntas() {
+  const navigate = useNavigate()
   const { allGames, handlerBorrarPregunta, } = useGlobalStore();
 
+  useEffect(() => {
+    if(allGames.length === 0) {
+      navigate("/")
+    }
+  },[allGames])
   return (
     <main className="flex gap-8">
       {allGames.map((g : Pregunta, key) => (
