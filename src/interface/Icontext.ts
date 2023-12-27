4ba6b5d1152407f6170f0646d87ff95f043ca6ac
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-27 13:34:35 -0300

    interface typescript

--- src/interface/Icontext.ts
+++ src/interface/Icontext.ts
@@ -1,9 +1,24 @@
 import { Pregunta } from "./Pregunta"
 
+export interface IRespuesta {
+    isCorrect: boolean;
+    respuesta?: string;
+  }
+
+
+export interface Juego {
+    pregunta: string
+    respuestas: IRespuesta[]
+  }
+
 export interface IContext {
     AccessoTerminadoTrue : () => void
     AccessoTerminadoFalse : () => void
     accesoTerminado: boolean
     allGames : Pregunta[]
     handlerBorrarPregunta : (pregunta : string) => void
+    handlerAgregarJuego : (e : React.FormEvent<HTMLFormElement> ) => void
+    juego: Juego
+    setJuego: any
+    
 }
