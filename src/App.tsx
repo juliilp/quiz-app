6039ae1ed78e2c34645004dffc4b08f168650b14
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-27 20:44:31 -0300

    saque ruta protegida de quizzgames

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,36 @@ import QuizGamesSore from "./store/QuizGamesStore";
 import AgregarJuego from "./vistas/AgregarJuego";
 import Resultados from "./vistas/Resultados";
 import BorrarPreguntas from "./vistas/BorrarPreguntas";
-import QuizGames from './vistas/QuizGames'
+import QuizGames from "./vistas/QuizGames";
+import { useEffect } from "react";
 export default function App() {
+
+  useEffect(() => {
+    const existingGames = localStorage.getItem("Games");
+    const typeGames = localStorage.getItem("TypeGame")
+    if (!existingGames) {
+      localStorage.setItem("Games", JSON.stringify([]));
+    }
+    if(!typeGames){
+      localStorage.setItem("TypeGame", JSON.stringify([]))
+    }
+  }, []);
   return (
     <QuizGamesSore>
       <BrowserRouter>
         <Navbar />
         <Routes>
+       
+            <Route path="/QuizGames/agregar-juego" element={<AgregarJuego />} />
+            <Route path="/QuizGames/resultados" element={<Resultados />} />
+            <Route
+              path="/QuizGames/borrarPreguntas"
+              element={<BorrarPreguntas />}
+            />
+            <Route path="/QuizGames" element={<QuizGames />} />
+         
+
           <Route path="/" element={<Home />} />
-          <Route path="/QuizGames/agregar-juego" element={<AgregarJuego />} />
-          <Route path="/QuizGames/resultados" element={<Resultados />} />
-          <Route path="/QuizGames/borrarPreguntas" element={<BorrarPreguntas />} />
-          <Route path="/QuizGames" element={<QuizGames />} />
         </Routes>
       </BrowserRouter>
     </QuizGamesSore>
