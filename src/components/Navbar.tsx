e6a6d28a2d9f25f6b0346edb8e47318d2c55ef89
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-27 20:45:02 -0300

    Navbar mostrando juegos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,25 @@
 import { Link } from "react-router-dom";
-import useGlobalStore from "../hooks/useGlobalStore";
-
+import useQuizGamesStore from "../hooks/useQuizGamesStore";
 export default function Navbar() {
-  const {AccessoTerminadoFalse, allGames} = useGlobalStore()
+  const { AccessoTerminadoFalse } = useQuizGamesStore();
+
+  const handlerInicio = () => {
+    AccessoTerminadoFalse();
+    localStorage.setItem("TypeGame", "");
+  };
+
   return (
-    <header className="w-full" >
-      <nav className="w-full" >
+    <header className="w-full">
+      <nav className="w-full">
         <ul className="flex items-center gap-4 justify-around h-16 shadow-xl">
           <li>
-            <Link to="/"
-            onClick={() => AccessoTerminadoFalse()}
-            >Inicio</Link>
+            <Link to="/" onClick={() => handlerInicio}>
+              Inicio
+            </Link>
           </li>
           <li>
-            <Link to="/QuizGames/agregar-juego"
-            onClick={() => AccessoTerminadoFalse()}
-            >Agregar juego</Link>
+            <Link to="/QuizGames">QuizGames</Link>
           </li>
-          {
-            allGames.length > 0 && <li>
-              <Link to="/QuizGames/borrarPreguntas" >
-              Borrar pregunta
-              </Link>
-            </li>
-          }
         </ul>
       </nav>
     </header>
