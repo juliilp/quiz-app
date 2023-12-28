0424ff0c9d7bb8be7cfeb31b7e697d2ae71db8dd
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-28 17:22:37 -0300

    ordenando opciones a navbar o pestaña

--- src/vistas/Chinchon.tsx
+++ src/vistas/Chinchon.tsx
@@ -2,41 +2,18 @@ import CardChinchon from "../components/CardChinchon";
 import useChinchonStore from "../hooks/useChinchonStore";
 
 export default function Chinchon() {
-  const {
-    onChangeCreateJugadores,
-    jugadorCreate,
-    handlerAddJugadores,
-    allJugadores
-  } = useChinchonStore();
+  const { allJugadores } = useChinchonStore();
 
-
-  const handlerCreateJugadores = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    handlerAddJugadores()
-  }
-  const ordenJugadores = [...allJugadores].sort((a, b) => a.nombre.localeCompare(b.nombre));
+  const ordenJugadores = [...allJugadores].sort((a, b) =>
+    a.nombre.localeCompare(b.nombre)
+  );
   return (
     <main>
-
-      <form onSubmit={handlerCreateJugadores}>
-        <input
-          type="text"
-          onChange={onChangeCreateJugadores}
-          value={jugadorCreate.nombre}
-        />
-        <button>Crear jugador</button>
-      </form>
-
-    <section className="flex gap-16" >
-      {
-       
-       ordenJugadores.map(({nombre,puntos}, key) => {
-          return <CardChinchon nombre={nombre} puntos={puntos} key={key}  />
-
-        })
-      }
-    </section>
-      
+      <section className="flex gap-16">
+        {ordenJugadores.map(({ nombre, puntos }, key) => {
+          return <CardChinchon nombre={nombre} puntos={puntos} key={key} />;
+        })}
+      </section>
     </main>
   );
 }
