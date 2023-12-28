acabb75e09d782be5e801dbaa252357e7957acef
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-28 11:51:01 -0300

    crear jugador funcionando

--- src/vistas/Chinchon.tsx
+++ src/vistas/Chinchon.tsx
@@ -1,5 +1,32 @@
+import useChinchonStore from "../hooks/useChinchonStore";
+
 export default function Chinchon() {
+  const {
+    onChangeCreateJugadores,
+    jugadorCreate,
+    handlerAddJugadores,
+    allJugadores
+  } = useChinchonStore();
+
+
+  const handlerCreateJugadores = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    handlerAddJugadores()
+  }
+  console.log(allJugadores)
   return (
-    <div>Chinchon</div>
-  )
+    <main>
+
+      <form onSubmit={handlerCreateJugadores}>
+        <input
+          type="text"
+          onChange={onChangeCreateJugadores}
+          value={jugadorCreate.nombre}
+        />
+        <button>Crear jugador</button>
+      </form>
+
+      
+    </main>
+  );
 }
