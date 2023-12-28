5d0c95f1ec0423f8ce184bc4456cedc0252df238
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-28 13:53:00 -0300

    guardando en el localstorage el tipo de juego

--- src/vistas/Home.tsx
+++ src/vistas/Home.tsx
@@ -1,13 +1,28 @@
 import { Link } from "react-router-dom";
 
 export default function Home() {
+  const selectQuizGames = () => {
+    localStorage.setItem("TypeGame", "QuizGames");
+  };
+
+  const selectChinchon = () => {
+    localStorage.setItem("TypeGame", "Chinchon");
+  };
+
+  const selectInicio = () => {
+    localStorage.setItem("TypGame", "");
+  };
   return (
     <main>
       <h1>Juegos</h1>
-        <article className="flex items-center gap-8" >
-        <Link to="/quizgames" >Quiz Games</Link>
-      <Link to="/Chinchon" >Chinchon</Link>
-        </article>
+      <article className="flex items-center gap-8">
+        <button onClick={selectQuizGames}>
+          <Link to="/quizgames">Quiz Games</Link>
+        </button>
+        <button onClick={selectChinchon}>
+          <Link to="/Chinchon">Chinchon</Link>
+        </button>
+      </article>
     </main>
-  )
+  );
 }
