b019ef8e2cdb735303e46826e91bb0b9d2a9fe3d
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-28 23:03:24 -0300

    si no hay jugadores, te redirecciona a crear un jugador

--- src/vistas/Chinchon.tsx
+++ src/vistas/Chinchon.tsx
@@ -1,12 +1,21 @@
+import { useEffect } from "react";
 import CardChinchon from "../components/CardChinchon";
 import useChinchonStore from "../hooks/useChinchonStore";
+import { useNavigate } from "react-router-dom";
 
 export default function Chinchon() {
+  const navigate = useNavigate()
   const { allJugadores } = useChinchonStore();
 
   const ordenJugadores = [...allJugadores].sort((a, b) =>
     a.nombre.localeCompare(b.nombre)
   );
+
+  useEffect(() => {
+    if(allJugadores.length === 0) {
+      return navigate("/Chinchon/crearjugador")
+    }
+  },[])
   return (
     <main>
       <section className="flex gap-16">
