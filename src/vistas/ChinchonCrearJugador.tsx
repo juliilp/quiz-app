54b343d86e45e72cbb37ccf7fa23c076f6bb408c
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-29 13:15:36 -0300

    ux boton

--- src/vistas/ChinchonCrearJugador.tsx
+++ src/vistas/ChinchonCrearJugador.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useState } from "react";
 import useChinchonStore from "../hooks/useChinchonStore";
 
 export default function ChinchonCrearJugador() {
+  const [errButton, setErrButton] = useState(true)
   const { onChangeCreateJugadores, jugadorCreate, handlerAddJugadores } =
     useChinchonStore();
 
@@ -10,15 +12,27 @@ export default function ChinchonCrearJugador() {
     handlerAddJugadores();
   };
 
+useEffect(() => {
+if(jugadorCreate.nombre.length  < 3) {
+  return setErrButton(true)
+}
+return setErrButton(false)
+},[errButton,jugadorCreate.nombre])
   return (
     <main>
-      <form onSubmit={handlerCreateJugadores}>
+      <form onSubmit={handlerCreateJugadores} className="flex flex-col gap-8 my-12 justify-center items-center">
         <input
           type="text"
           onChange={onChangeCreateJugadores}
           value={jugadorCreate.nombre}
+          placeholder="Nombre del jugador.."
+          className="outline-2 py-2 pl-4 mb-8 relative w-[300px] "
         />
-        <button>Crear jugador</button>
+        <button
+        className={` ${
+          errButton ? "cursor-not-allowed" : "cursor-pointer"
+        } mt-6 rounded-lg py-2 px-6 md:px-10 font-bold text-white bg-red-600`}
+        >Crear jugador</button>
       </form>
     </main>
   );
