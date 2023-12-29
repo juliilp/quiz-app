130c29b7bffdc7b7aef0968f63208f89444ef6c5
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-29 12:22:40 -0300

    una idea de home

--- src/vistas/Home.tsx
+++ src/vistas/Home.tsx
@@ -1,18 +1,42 @@
 import { Link } from "react-router-dom";
-import useQuizGamesStore from '../hooks/useQuizGamesStore'
+import useQuizGamesStore from "../hooks/useQuizGamesStore";
 export default function Home() {
-  const {selectQuizGames,selectChinchon} = useQuizGamesStore()
+  const { selectQuizGames, selectChinchon } = useQuizGamesStore();
+
   return (
     <main>
-      <h1>Juegos</h1>
-      <article className="flex items-center gap-8">
-        <button onClick={selectQuizGames}>
-          <Link to="/quizgames">Quiz Games</Link>
-        </button>
-        <button onClick={selectChinchon}>
-          <Link to="/Chinchon">Chinchon</Link>
-        </button>
-      </article>
+      <section className="flex flex-col gap-8 mt-8 ml-6" >
+        <article>
+          <button onClick={selectQuizGames}>
+            <Link to="/quizgames">
+              <u className="animate-pulse text-3xl font-semibold ">
+                Quiz Games
+              </u>
+            </Link>
+          </button>
+          <p className="max-w-[350px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            sunt molestiae officiis vel non! Architecto, possimus voluptatem
+            necessitatibus dolorem cupiditate odio at officia unde voluptatum.
+            Fuga harum possimus nesciunt ullam.
+          </p>
+        </article>
+
+        <article>
+          <button onClick={selectChinchon}>
+            <Link to="/Chinchon">
+              <u className="animate-pulse text-3xl font-semibold">Chinchon</u>
+            </Link>
+          </button>
+
+          <p className="max-w-[350px]">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            sunt molestiae officiis vel non! Architecto, possimus voluptatem
+            necessitatibus dolorem cupiditate odio at officia unde voluptatum.
+            Fuga harum possimus nesciunt ullam.
+          </p>
+        </article>
+      </section>
     </main>
   );
 }
