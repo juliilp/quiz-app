6a3423878f9dde58f51bd53b60f6423a66332b97
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-30 17:42:15 -0300

    saque redireccion

--- src/vistas/Chinchon.tsx
+++ src/vistas/Chinchon.tsx
@@ -1,22 +1,14 @@
-import { useEffect } from "react";
 import CardChinchon from "../components/CardChinchon";
 import useChinchonStore from "../hooks/useChinchonStore";
-import { useNavigate } from "react-router-dom";
 import ChinchonCrearJugador from "./ChinchonCrearJugador";
 
 export default function Chinchon() {
-  const navigate = useNavigate();
   const { allJugadores } = useChinchonStore();
 
   const ordenJugadores = [...allJugadores].sort((a, b) =>
     a.nombre.localeCompare(b.nombre)
   );
 
-  useEffect(() => {
-    if (allJugadores.length === 0) {
-      return navigate("/Chinchon/crearjugador");
-    }
-  }, []);
   return (
     <main>
       <ChinchonCrearJugador />
