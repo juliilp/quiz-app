3d903a3628ef0a22fb23cfa559e29f82543217a8
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-30 17:19:50 -0300

    agregue crear jugador acá

--- src/vistas/Chinchon.tsx
+++ src/vistas/Chinchon.tsx
@@ -2,9 +2,10 @@ import { useEffect } from "react";
 import CardChinchon from "../components/CardChinchon";
 import useChinchonStore from "../hooks/useChinchonStore";
 import { useNavigate } from "react-router-dom";
+import ChinchonCrearJugador from "./ChinchonCrearJugador";
 
 export default function Chinchon() {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { allJugadores } = useChinchonStore();
 
   const ordenJugadores = [...allJugadores].sort((a, b) =>
@@ -12,12 +13,13 @@ export default function Chinchon() {
   );
 
   useEffect(() => {
-    if(allJugadores.length === 0) {
-      return navigate("/Chinchon/crearjugador")
+    if (allJugadores.length === 0) {
+      return navigate("/Chinchon/crearjugador");
     }
-  },[])
+  }, []);
   return (
     <main>
+      <ChinchonCrearJugador />
       <section className="grid justify-center items-center md:grid-cols-2 lg:grid-cols-3 my-12 gap-12">
         {ordenJugadores.map(({ nombre, puntos }, key) => {
           return <CardChinchon nombre={nombre} puntos={puntos} key={key} />;
