1cb26d696d090f02cc54c2709d97d4a6370a4601
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-30 17:20:16 -0300

    boton e input ux

--- src/vistas/ChinchonCrearJugador.tsx
+++ src/vistas/ChinchonCrearJugador.tsx
@@ -19,19 +19,19 @@ if(jugadorCreate.nombre.length  < 3) {
 return setErrButton(false)
 },[errButton,jugadorCreate.nombre])
   return (
-    <main>
-      <form onSubmit={handlerCreateJugadores} className="flex flex-col gap-8 my-12 justify-center items-center">
+    <main className="w-full flex items-center justify-center mt-16" >
+      <form onSubmit={handlerCreateJugadores} className="flex gap-4 items-center justify-center" >
         <input
           type="text"
           onChange={onChangeCreateJugadores}
           value={jugadorCreate.nombre}
           placeholder="Nombre del jugador.."
-          className="outline-2 py-2 pl-4 mb-8 relative w-[300px] "
+          className="outline-2 py-2 pl-4 relative w-[300px] h-max border border-black rounded-lg "
         />
         <button
         className={` ${
           errButton ? "cursor-not-allowed" : "cursor-pointer"
-        } mt-6 rounded-lg py-2 px-6 md:px-10 font-bold text-white bg-red-600`}
+        } rounded-lg py-2 px-6 md:px-10 font-bold text-white bg-red-600  `}
         >Crear jugador</button>
       </form>
     </main>
