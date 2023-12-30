8c8c5132a2c88eb03b9a901eacdb2df9a7bb1c7e
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-30 17:19:29 -0300

    mejore exp de usuario

--- src/components/CardChinchon.tsx
+++ src/components/CardChinchon.tsx
@@ -8,11 +8,10 @@ interface Props {
 
 export default function CardChinchon({ nombre, puntos }: Props) {
   const [puntosTotal, setPuntosTotal] = useState<number>(0);
-  const [newPuntos, setNewPuntos] = useState<number>(0);
-
+  const [newPuntos, setNewPuntos] = useState<string>("");
   const onChangeNewPuntos = (e: React.ChangeEvent<HTMLInputElement>) => {
     const number = e.target.value;
-    setNewPuntos(Number(number));
+    setNewPuntos(number);
   };
 
   const { sumarPuntos, handlerRetroceder, borrarJugador, hacerMenos10 } =
@@ -20,38 +19,43 @@ export default function CardChinchon({ nombre, puntos }: Props) {
 
   const handlerSumarPuntos = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    sumarPuntos(nombre, newPuntos);
+    sumarPuntos(nombre, Number(newPuntos));
+    setNewPuntos("");
   };
 
   useEffect(() => {
     setPuntosTotal(puntos.reduce((acc, val) => acc + val, 0));
   }, [puntos]);
 
-  const primerLetra = nombre.split("")[0].toUpperCase()
-  const restoDeLetras = primerLetra + nombre.slice(1, -1).toLowerCase()
+  const primerLetra = nombre.split("")[0].toUpperCase();
+  const restoDeLetras = primerLetra + nombre.slice(1, -1).toLowerCase();
 
   return (
     <article className="flex flex-col relative  w-[300px] p-4 border border-black">
-      <h2 className="mb-6" >{restoDeLetras}</h2>
+      <h2 className="mb-6">{restoDeLetras}</h2>
       <form onSubmit={handlerSumarPuntos}>
         <input
           type="number"
           onChange={onChangeNewPuntos}
           className="border border-black"
+          value={newPuntos}
         />
-  
-      <article className="flex gap-4 items-center mt-2" >
-        <button  type="button" onClick={() => handlerRetroceder(nombre)}>Retroceder</button>
-      <button  type="button" onClick={() => hacerMenos10(nombre)}>-10</button>
-      <button type="submit" >Sumar</button>
-      </article>
+
+        <article className="flex gap-4 items-center mt-2">
+          <button type="button" onClick={() => handlerRetroceder(nombre)}>
+            Retroceder
+          </button>
+          <button type="button" onClick={() => hacerMenos10(nombre)}>
+            -10
+          </button>
+          <button type="submit">Sumar</button>
+        </article>
       </form>
 
       <article className="absolute top-0 right-2 flex gap-4 ">
-        <span className="block text-lg">{puntosTotal}</span>
+        <h4 className="text-lg">Puntos: {puntosTotal}</h4>
         <button onClick={() => borrarJugador(nombre)}>❌</button>
       </article>
-
     </article>
   );
 }
