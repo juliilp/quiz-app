b7bb13c15da3928a789df4b0fea198e3545e6ed7
Julian Lopez Padua <julianlopez43013@gmail.com>
2023-12-31 13:18:27 -0300

    le agregue un confirm

--- src/components/CardBorrarPregunta.tsx
+++ src/components/CardBorrarPregunta.tsx
@@ -7,10 +7,19 @@ interface Props {
 }
 
 const CardBorrarPregunta: React.FC<Props> = ({ pregunta, index, onBorrar }) => {
+
+  const handlerBorrar = () => {
+    const res = confirm("Estas seguro de borrar la pregunta: " + pregunta + " ?")
+    if(res) {
+      onBorrar(pregunta)
+    }
+  }
   return (
-    <article key={index} className="border w-[200px] h-[200px] border-black">
-      <h2>{pregunta}</h2>
-      <button onClick={() => onBorrar(pregunta)}>
+    <article key={index} className="border w-[250px] max-w-[350px] p-6 border-black flex flex-col gap-8">
+      <h2 className="text-sm" >{pregunta}</h2>
+      <button onClick={handlerBorrar}
+      className="rounded-lg py-2 px-6 md:px-10 font-bold bg-[#065a82]"
+      >
         Borrar Pregunta
       </button>
     </article>
